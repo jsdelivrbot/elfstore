dc78fd263dfadd17a04ffd67468d429cb3fba80d
atellmer <atellmer@gmail.com>
2017-02-02 16:51:34 +0500

    gulp-task styles modified

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -7,15 +7,31 @@ const connect = require('gulp-connect');
 const concat = require('gulp-concat');
 const config = require('../config');
 
-//styl
-gulp.task('styles', () => {
-	return gulp.src([
-		`${config.path.shared}/**/*.styl`,
-		`${config.path.components}/**/*.styl`])
-		.pipe(concat('bundle.styl'))
-		.pipe(stylus({
-			use: [nib()],
-			compress: false
-		}))
-		.pipe(gulp.dest(config.path.dist));
-});
+
+if (config.development) {
+  gulp.task('styles', () => {
+    return gulp.src([
+      `${config.path.shared}/**/*.styl`,
+      `${config.path.components}/**/*.styl`])
+      .pipe(concat('bundle.styl'))
+      .pipe(stylus({
+        use: [nib()],
+        compress: false
+      }))
+      .pipe(gulp.dest(config.path.dist));
+  });
+}
+
+if (config.production) {
+  gulp.task('styles', () => {
+    return gulp.src([
+      `${config.path.shared}/**/*.styl`,
+      `${config.path.components}/**/*.styl`])
+      .pipe(stylus({
+        use: [nib()],
+        compress: false
+      }))
+      .pipe(gulp.dest(file => file.base));
+  });
+}
+
