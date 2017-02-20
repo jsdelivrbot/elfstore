08ab35ee52d6e48458fff0abc61c7262dfa4156f
atellmer <atellmer@gmail.com>
2017-02-20 17:51:03 +0500

    product-page started

---
+++ client/components/zoom-gallery/zoom-gallery.component.js
@@ -0,0 +1,37 @@
+;
+(function () {
+	'use strict';
+
+	document.addEventListener('DOMContentLoaded', ready);
+
+	function ready() {
+		zoomGalleryInit();
+
+		window.addEventListener('resize', resizeHandler);
+
+		function resizeHandler() {
+			zoomGalleryDestroy();
+			zoomGalleryInit();
+		}
+
+		function zoomGalleryInit() {
+			$('#zoom-gallery-root').ezPlus({
+				zoomType: 'inner',
+				containLensZoom: true,
+				gallery: 'zoom-gallery-thumbnails',
+				cursor: 'crosshair',
+				galleryActiveClass: 'active',
+				borderSize: 2,
+				borderColour: '#ffde13',
+				galleryItem: 'span',
+				preloading: 0,
+				zIndex: 10,
+				responsive: true
+			});
+		}
+
+		function zoomGalleryDestroy() {
+			$('.zoomContainer').remove();
+		}
+	}
+})();
