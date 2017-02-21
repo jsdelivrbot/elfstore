189196e9f7a92e8e1a0ff5b13c4666837db79ee1
atellmer <atellmer@gmail.com>
2017-02-21 17:36:45 +0500

    new build

--- components/zoom-gallery/zoom-gallery.component.js
+++ components/zoom-gallery/zoom-gallery.component.js
@@ -5,16 +5,38 @@
 	document.addEventListener('DOMContentLoaded', ready);
 
 	function ready() {
+		var bg, firstStart = true;
 		zoomGalleryInit();
 
 		window.addEventListener('resize', resizeHandler);
 
 		function resizeHandler() {
-			zoomGalleryDestroy();
-			zoomGalleryInit();
+			getBackgroundImage()
+				.then(function (bg) {
+					zoomGalleryDestroy()
+						.then(function() {
+							zoomGalleryInit(bg);
+						});
+				});
 		}
 
-		function zoomGalleryInit() {
+		function getBackgroundImage() {
+			return new Promise(function (resolve) {
+				var bg = $('.zoomWindow').css('background-image');
+
+				if (bg) {
+					resolve(bg);
+				}
+			});
+		}
+
+		function replaceBackgroundImage(bg) {
+			if (bg) {
+				$('.zoomWindow').css('background-image', bg);
+			}
+		}
+
+		function zoomGalleryInit(bg) {
 			$('#zoom-gallery-root').ezPlus({
 				zoomType: 'inner',
 				containLensZoom: true,
@@ -26,12 +48,27 @@
 				galleryItem: 'span',
 				zIndex: 10,
 				responsive: true,
-				preloading: 1
+				onZoomedImageLoaded: function () {
+					if (!firstStart) {
+						replaceBackgroundImage(bg);
+					}
+					firstStart = false;
+				}
 			});
 		}
 
 		function zoomGalleryDestroy() {
-			$('.zoomContainer').remove();
+			return new Promise(function (resolve) {
+				var len = $('.zoomContainer').length,
+						i;
+
+				for (i = 0; i < len; i++) {
+					$('.zoomContainer').remove();
+				}
+				if ($('.zoomContainer').length === 0) {
+					resolve();
+				}
+			});
 		}
 	}
 })();
