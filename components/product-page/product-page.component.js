e940b8d4a10d381279f530c7f7a7ab9d572e924b
atellmer <atellmer@gmail.com>
2017-02-22 16:35:51 +0500

    new build

--- components/product-page/product-page.component.js
+++ components/product-page/product-page.component.js
@@ -5,5 +5,36 @@
 	document.addEventListener('DOMContentLoaded', ready);
 
 	function ready() {
+		var options = {
+			allowedTypes: 'png|jpg|jpeg|gif',
+			animationSpeed: 250,
+			preloadNext: true,
+			enableKeyboard: true,
+			quitOnEnd: false,
+			quitOnImgClick: false,
+			quitOnDocClick: true,
+			onStart: overlayOn,
+			onEnd: overlayOff,
+			onLoadStart: activityIndicatorOn,
+			onLoadEnd: activityIndicatorOff
+		};
+
+		$('[data-product-feedback-item]').imageLightbox(options);
+
+		function overlayOn() {
+			$('<div id="imagelightbox-overlay"></div>').appendTo('body');
+		}
+
+		function overlayOff() {
+			$('#imagelightbox-overlay').remove();
+		}
+
+		function activityIndicatorOn() {
+			$( '<div id="imagelightbox-loading"><div></div></div>' ).appendTo( 'body' );
+		}
+
+		function activityIndicatorOff() {
+			$( '#imagelightbox-loading' ).remove();
+		}
 	}
 })();
